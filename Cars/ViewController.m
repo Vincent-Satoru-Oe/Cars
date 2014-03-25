f9df6a0ddfe115db914187f7c2f0869fb731b5c9
Vincent Oe <vincent.satoru.oe@gmail.com>
2014-03-25 14:12:13 -0700

    pragramatically spawned initial car

--- Cars/ViewController.m
+++ Cars/ViewController.m
@@ -41,7 +41,6 @@
     road3.center = CGPointMake(road3.center.x, road3.center.y + 10);
     road2.center = CGPointMake(road2.center.x, road2.center.y + 10);
     road1.center = CGPointMake(road1.center.x, road1.center.y + 10);
-    [playerCar drawAtPoint:CGPointMake(199, 448)];
 
     leftLaneCar1.center = CGPointMake(leftLaneCar1.center.x, leftLaneCar1.center.y + 10);
     leftLaneCar2.center = CGPointMake(leftLaneCar2.center.x, leftLaneCar2.center.y + 10);
@@ -132,16 +131,16 @@
     road2.center = CGPointMake(160, 484);
     road1.center = CGPointMake(160, 548);
     
-    testCar1.center = CGPointMake(38, 250);
-    testCar2.center = CGPointMake(116, 250);
-    testCar3.center = CGPointMake(205, 250);
-    testCar4.center = CGPointMake(285, 250);
+    //testCar1.center = CGPointMake(38, 250);
+    //testCar2.center = CGPointMake(116, 250);
+    //testCar3.center = CGPointMake(205, 250);
+    //testCar4.center = CGPointMake(285, 250);
     
+    //playerCar = [UIImage imageNamed:@"BlueCar.png"];
+    //playersCar.center = CGPointMake(199, 448);
+    //playerCarView = [[UIImageView alloc] initWithImage:playerCar];
+    //[self.view addSubview:playerCarView];
     
-    playersCar.center = CGPointMake(199, 448);
-    
-    playerCar = [UIImage imageWithContentsOfFile:@"BlueCar"];
-    [playerCar drawAtPoint:CGPointMake(199, 448)];
 }
 
 - (void)viewDidLoad
@@ -151,8 +150,9 @@
     playersCar.hidden = NO;
     
     
-    playerCar = [UIImage imageNamed:@"MyImage.png"];
+    playerCar = [UIImage imageNamed:@"BlueCar.png"];
     playerCarView = [[UIImageView alloc] initWithImage:playerCar];
+    playerCarView.center = CGPointMake(199, 448);
     [self.view addSubview:playerCarView];
     // Add imageView to a parent view here.
     
